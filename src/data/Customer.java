1d3d5e038a2801d13fee3c1130f75e4e0262bd04
Ashwani Rajput <ashwanir461@gmail.com>
2022-08-16 15:13:49 +0530

    final code

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -1,5 +1,74 @@
 package data;
 
 public class Customer {
+    private String name;
+    private long phoneNumber;
+    private String emailId;
+    private String address;
+    private boolean premiumMember;
 
+    public Customer() {
+        this.name = "john";
+        this.phoneNumber = 875469558;
+        this.emailId = "john123@gmail.com";
+        this.address = "Dhampur Bijnor";
+        this.premiumMember = true;
+    }
+
+    public Customer(String name, long phoneNumber, String emailId, String address, boolean primumMemer) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        this.address = address;
+        premiumMember = primumMemer;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isPremiumMember() {
+        return premiumMember;
+    }
+
+    public void setPremiumMember(boolean premiumMember) {
+        this.premiumMember = premiumMember;
+    }
+
+    public void displayCustomer() {
+        System.out.println("name = " + name);
+        System.out.println("phoneNumber = " + phoneNumber);
+        System.out.println("emailId = " + emailId);
+        System.out.println("address = " + address);
+        System.out.println("primumMemer = " + premiumMember);
+    }
 }
--- src/main/CustomerImpl.java
+++ src/main/CustomerImpl.java
@@ -1,5 +1,42 @@
 package main;
 
+import data.Customer;
+
 public class CustomerImpl {
+    public static void main(String[] args) {
+        //creating object for parameterized constructor
+        Customer object1 = new Customer("Ashwani", 85647123, "ashwanir41@gmail.com", "Dhampur", true);
+        //creating object for non-parameterized constructor
+        Customer object2 = new Customer();
+        //display Attribute value
+
+        System.out.println("Print attribute value for the object1 parameterized constructor \n ");
+        object1.displayCustomer();
+
+        System.out.println("Print attribute value for the object2 non-parameterized constructor \n ");
+        object2.displayCustomer();
+
+        System.out.println("Print attribute value for the getter and getter method \n");
+        object2.setName("tom");
+        System.out.println("object2.getName() = " + object2.getName());
+        object2.setPhoneNumber(875324163);
+        System.out.println("object2.getPhoneNumber() = " + object2.getPhoneNumber());
+        object2.setEmailId("tom123@gmail.com");
+        System.out.println("object2.getEmailId() = " + object2.getEmailId());
+        object2.setAddress("India");
+        System.out.println("object2.getAddress() = " + object2.getAddress());
+        object2.setPremiumMember(false);
+        System.out.println("object2.isPremiumMember() = " + object2.isPremiumMember());
+
+        System.out.println("Print change there reference object1 will display the value of object2 and object 2 will display the value of object1\n");
+        // using swapping the object of the class
+        Customer tempObject = new Customer();
+        tempObject = object1;
+        object1 = object2;
+        object1.displayCustomer();
+        object2 = tempObject;
+        System.out.println();
+        object2.displayCustomer();
+    }
 
 }
